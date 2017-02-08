3ef55c9188b9123313b54da961296ed2cd07326f
Sean Lee <freshdried@gmail.com>
2017-02-07 22:35:03 -0500

    fix type

--- 0_code/src/StoreDef.js
+++ 0_code/src/StoreDef.js
@@ -8,7 +8,7 @@ import type {Reducer} from 'redux'
 
 // TODO: switch to a Class interface, and use this
 
-type ActionsType<S, O> = $ObjMap<O, <P>(v: P => S) => (payload: P, error: ?Error) => empty>;
+type ActionsType<S, O> = $ObjMap<O, <P>(v: P => S) => ((payload: P, error: ?Error) => Object)>;
 type ReducerType<S, O> = Reducer<S, {type: $Keys<O>, payload: ?any, error: ?any}>;
 
 type OutputType<S, O> = {
@@ -16,7 +16,7 @@ type OutputType<S, O> = {
   reducer: ReducerType<S, O>
 };
 
-const makeActionsFromStoreDef = <S, O: *> (makeActionsObj: S => O): ActionsType<S, O> => {
+const makeActionsFromStoreDef = <S, O: {}> (makeActionsObj: S => O): ActionsType<S, O> => {
   const keys = Object.keys((makeActionsObj: any)());
   const actions = {};
   for (let i = 0; i < keys.length; i++) {
@@ -25,14 +25,14 @@ const makeActionsFromStoreDef = <S, O: *> (makeActionsObj: S => O): ActionsType<
       return {
         type: key,
         payload: payload,
-        error: error
+        error: error,
       };
     };
   }
   return actions;
 }
 
-const makeReducerFromStoreDef = <S, O: *> (initialState: S, makeActionsObj: S => O): ReducerType<S, O> => {
+const makeReducerFromStoreDef = <S, O: {}> (initialState: S, makeActionsObj: S => O): ReducerType<S, O> => {
   const actionsObj = {};
   const keys = Object.keys((makeActionsObj: any)());
   for (let i = 0; i < keys.length; i++) {
@@ -52,7 +52,7 @@ const makeReducerFromStoreDef = <S, O: *> (initialState: S, makeActionsObj: S =>
   return reducer;
 }
 
-export const makeStoreDef = <S, O: *>(initialState: S, makeActionsObj: S => O): OutputType<S, O> => ({
+export const makeStoreDef = <S, O: {}>(initialState: S, makeActionsObj: S => O): OutputType<S, O> => ({
   actions: makeActionsFromStoreDef(makeActionsObj),
   reducer: makeReducerFromStoreDef(initialState, makeActionsObj),
 });
