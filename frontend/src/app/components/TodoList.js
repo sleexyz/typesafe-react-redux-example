55006962deb4906c202d98693a91d56552f051f6
Sean Lee <freshdried@gmail.com>
2017-02-14 14:01:47 -0500

    styled-components init

--- frontend/src/app/components/TodoList.js
+++ frontend/src/app/components/TodoList.js
@@ -1,18 +1,12 @@
 // @flow
 import React from 'react';
-import { connect, dispatch } from 'app/redux';
+import { connect } from 'app/redux';
 import type { State } from 'app/state/Todo';
-import { actions, TodoLens } from 'app/state/Todo';
+import { TodoLens } from 'app/state/Todo';
 import TodoListEntry from 'app/components/TodoListEntry';
-
-const addOnClick = () => {
-  dispatch(actions.createTodo());
-};
 
 const TodoList = ({ todos }: State) => (
   <div>
-    <h2>TodoList</h2>
-    <button onClick={addOnClick} >Add</button>
     {todos.map((data, i) => <TodoListEntry {...data} index={i} key={data.id} />)}
   </div>
 );
